ec087aaf3b5a990ef0956c37dfa64cb955b843b6
Edwing Batz <ebatzm@miumg.edu.gt>
2022-08-31 19:59:36 -0600

    en la function capturar(), se le agg un if/else para la alerta al dejar una casilla vacia y querer capturar

--- web/js/script_2.js
+++ web/js/script_2.js
@@ -5,10 +5,35 @@
 
 function capturar(){
     var cod=document.getElementById("codigo").value;
-  var nom=document.getElementById("nombre").value;
+    var nom=document.getElementById("nombre").value;
     var ape=document.getElementById("apellido").value;
+    var mail=document.getElementById("correo").value;
     var dir=document.getElementById("direccion").value;
     var tel=document.getElementById("telefono").value;
-     
+        if (cod===""){
+            alert("Ingresar el Código");
+           document.getElementById("codigo").focus();
+        }else {
+            if (nom===""){
+            alert("Ingresar un Nombre");
+           document.getElementById("nombre").focus();
+        } else{
+            if (ape===""){
+            alert("Ingresar un Apellido");
+           document.getElementById("apellido").focus();
+        } else {
+            if (mail===""){
+            alert("Ingresar un Correo Electrónico");
+           document.getElementById("correo").focus();
+        } else {
+            if (dir===""){
+            alert("Ingresar una dirección domiciliar");
+           document.getElementById("direccion").focus();
+        } else {
+            if (tel===""){
+            alert("Ingresar un Número de Contacto");
+           document.getElementById("telefono").focus();
+        }}}}}}
+            
         console.log(cod, nom, ape, dir, tel);
 }
