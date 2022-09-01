f0859603b3d3c6d1d2432aa74bef02efc4f156c3
Edwing Batz <ebatzm@miumg.edu.gt>
2022-08-31 19:42:25 -0600

    Se creo la function Captura, para la obtención de los datos por js

---
+++ web/js/script_2.js
@@ -0,0 +1,14 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+function capturar(){
+    var cod=document.getElementById("codigo").value;
+  var nom=document.getElementById("nombre").value;
+    var ape=document.getElementById("apellido").value;
+    var dir=document.getElementById("direccion").value;
+    var tel=document.getElementById("telefono").value;
+     
+        console.log(cod, nom, ape, dir, tel);
+}
